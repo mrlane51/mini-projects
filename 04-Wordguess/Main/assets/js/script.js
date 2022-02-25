156f89b9836bf3641bca32ad635dacd27cc87597
Lane Lopez <mrlane51@ymail.com>
2022-02-25 03:56:02 -0600

    adding another mini

---
+++ 04-Wordguess/Main/assets/js/script.js
@@ -0,0 +1,19 @@
+const start = document.querySelector('#start');
+const timerEl = document.getElementById('timer');
+const blankEl = document.getElementById('#blank');
+const winsEl = document.getElementById('winsCount');
+const lossEl = document.getElementById('lossCount');
+
+const words = ['database', 'server', 'javascript', 'python', 'module'];
+const choice = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+const wins = [];
+const losses = [];
+
+function startGame() {
+  
+}
+
+start.addEventListener('click', function startGame() {
+console.log('started')
+})
